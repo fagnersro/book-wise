d125274b4fb5b277dd3abea47cc42e795d25ba74
fagnersro <fagner.hg40@gmail.com>
2023-05-24 13:50:15 -0300

    visual structure - bg hero tailwind

--- web/book-wise/src/app/page.tsx
+++ web/book-wise/src/app/page.tsx
@@ -1,20 +1,9 @@
-import Image from 'next/image'
-import bgHero from '../assets/bg-hero.png'
 import { Login } from '@/components/Login'
 
 export default function Home() {
   return (
     <div className="grid h-[58.25] w-screen grid-cols-2 p-[1.25rem]">
-      <div className="h-[57rem] w-[37.375rem] rounded-lg object-cover">
-        <Image
-          src={bgHero}
-          width={598}
-          height={912}
-          quality={100}
-          priority
-          alt="hero"
-        />
-      </div>
+      <div className="h-[57rem] w-[37.375rem] rounded-lg bg-[url(../assets/bg-hero.png)] bg-cover"></div>
 
       <div className="flex h-full items-center justify-center">
         <Login />
